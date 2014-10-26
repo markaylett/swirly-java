9cad7c0df6bd2c0e7bf7710426512018ba976e52
Mark Aylett <mark.aylett@gmail.com>
2014-10-26 21:10:40 +0000

    Moved engine classes to fig.

--- doobry-elm/src/main/java/org/doobry/mock/MockModel.java
+++ doobry-elm/src/main/java/org/doobry/mock/MockModel.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+ * Copyright (C) 2013, 2014 Mark Aylett <mark.aylett@gmail.com>
+ *
+ * All rights reserved.
+ *******************************************************************************/
 package org.doobry.mock;
 
 import org.doobry.domain.Asset;
