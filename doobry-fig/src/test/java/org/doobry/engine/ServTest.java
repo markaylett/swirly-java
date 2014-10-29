42eae522d125f8301d17d58415219533cdfeae7a
Mark Aylett <mark.aylett@gmail.com>
2014-10-29 05:47:59 +0000

    Extended unit test.

--- doobry-fig/src/test/java/org/doobry/engine/ServTest.java
+++ doobry-fig/src/test/java/org/doobry/engine/ServTest.java
@@ -5,6 +5,9 @@
  *******************************************************************************/
 package org.doobry.engine;
 
+import static org.doobry.util.Date.ymdToJd;
+
+import org.doobry.domain.Action;
 import org.doobry.domain.Reg;
 import org.doobry.mock.MockBank;
 import org.doobry.mock.MockJourn;
@@ -12,12 +15,15 @@ import org.doobry.mock.MockModel;
 import org.junit.Test;
 
 public final class ServTest {
-
     @Test
     public final void test() {
         final Serv s = new Serv(new MockBank(Reg.values().length), new MockJourn());
         try {
             s.load(new MockModel());
+            final Accnt trader = s.getLazyAccnt("WRAMIREZ");
+            final Accnt giveup = s.getLazyAccnt("DBRA");
+            final Book book = s.getLazyBook("EURUSD", ymdToJd(2014, 3, 14));
+            s.placeOrder(trader, giveup, book, null, Action.BUY, 12345, 1, 1);
         } finally {
             s.close();
         }
