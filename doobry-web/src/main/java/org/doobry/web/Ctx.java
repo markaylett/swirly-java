1960a061961d8b11bee03a74bddbaeeb927ab40e
Mark Aylett <mark.aylett@gmail.com>
2014-10-30 21:29:32 +0000

    Basic Json formatting.

--- doobry-web/src/main/java/org/doobry/web/Ctx.java
+++ doobry-web/src/main/java/org/doobry/web/Ctx.java
@@ -10,25 +10,49 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.doobry.domain.Rec;
+import org.doobry.domain.RecType;
+import org.doobry.domain.Reg;
+import org.doobry.engine.Serv;
+import org.doobry.mock.MockBank;
+import org.doobry.mock.MockJourn;
+import org.doobry.mock.MockModel;
+import org.doobry.util.SlNode;
+
 public final class Ctx {
     private static class CtxHolder {
         private static final Ctx INSTANCE = new Ctx();
     }
 
-    private final ExecutorService pool = Executors.newFixedThreadPool(1);
+    private final ExecutorService pool;
+    private final Serv serv;
 
     private Ctx() {
+        pool = Executors.newFixedThreadPool(1);
+        serv = new Serv(new MockBank(Reg.values().length), new MockJourn());
+        serv.load(new MockModel());
     }
 
     public static Ctx getInstance() {
         return CtxHolder.INSTANCE;
     }
 
-    public final Future<String> foo() {
+    public final Future<String> getRec(final RecType type) {
         return pool.submit(new Callable<String>() {
             @Override
             public final String call() {
-                return "foo";
+                final StringBuilder sb = new StringBuilder();
+                sb.append('[');
+                SlNode node = serv.getFirstRec(type);
+                for (int i = 0; node != null; node = node.slNext()) {
+                    final Rec rec = (Rec) node;
+                    if (i++ > 0) {
+                        sb.append(',');
+                    }
+                    rec.print(sb);
+                }
+                sb.append(']');
+                return sb.toString();
             }
         });
     }
--- doobry-web/src/test/java/org/doobry/web/CtxTest.java
+++ doobry-web/src/test/java/org/doobry/web/CtxTest.java
@@ -7,11 +7,12 @@ package org.doobry.web;
 
 import java.util.concurrent.ExecutionException;
 
+import org.doobry.domain.RecType;
 import org.junit.Test;
 
 public class CtxTest {
     @Test
     public final void test() throws InterruptedException, ExecutionException {
-        System.out.println(Ctx.getInstance().foo().get());
+        System.out.println(Ctx.getInstance().getRec(RecType.CONTR).get());
     }
 }
