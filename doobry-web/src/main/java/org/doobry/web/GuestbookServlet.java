c75e6c53f7222ed2260c3e28798bcd1ea214337b
Mark Aylett <mark.aylett@gmail.com>
2014-10-31 19:48:38 +0000

    New ctx methods.

--- doobry-web/src/main/java/org/doobry/web/GuestbookServlet.java
+++ doobry-web/src/main/java/org/doobry/web/GuestbookServlet.java
@@ -5,39 +5,38 @@
  *******************************************************************************/
 package org.doobry.web;
 
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-
 import java.io.IOException;
-import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.doobry.domain.RecType;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class GuestbookServlet extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        if (req.getParameter("testing") == null) {
-            resp.setContentType("text/plain");
-            resp.getWriter().println("Hello, this is a testing servlet. \n\n");
-            Properties p = System.getProperties();
-            p.list(resp.getWriter());
+    public final void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        } else {
-            UserService userService = UserServiceFactory.getUserService();
-            User currentUser = userService.getCurrentUser();
+        final UserService userService = UserServiceFactory.getUserService();
+        final User currentUser = userService.getCurrentUser();
+        if (currentUser == null) {
+            resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+            return;
+        }
 
-            if (currentUser != null) {
-                resp.setContentType("text/plain");
-                resp.getWriter().println("Hello, " + currentUser.getNickname());
-            } else {
-                resp.sendRedirect(userService.createLoginURL(req
-                        .getRequestURI()));
-            }
+        final Future<CharSequence> fsb = Ctx.getInstance().getRec(RecType.CONTR);
+        resp.setContentType("application/json");
+        try {
+            resp.getWriter().append(fsb.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
     }
 }
--- doobry-web/src/test/java/org/doobry/web/CtxTest.java
+++ doobry-web/src/test/java/org/doobry/web/CtxTest.java
@@ -7,12 +7,21 @@ package org.doobry.web;
 
 import java.util.concurrent.ExecutionException;
 
+import org.doobry.domain.Action;
 import org.doobry.domain.RecType;
 import org.junit.Test;
 
 public class CtxTest {
     @Test
     public final void test() throws InterruptedException, ExecutionException {
-        System.out.println(Ctx.getInstance().getRec(RecType.CONTR).get());
+        System.out.println(Ctx.getInstance().getRec(RecType.ASSET).get());
+        System.out.println(Ctx.getInstance().getRecByMnem(RecType.CONTR, "EURUSD").get());
+        System.out.println(Ctx
+                .getInstance()
+                .postOrderByAccnt("WRAMIREZ", "DBRA", "EURUSD", 20141031, "test", Action.BUY,
+                        12345, 5, 1).get());
+        System.out.println(Ctx.getInstance().getOrderByAccnt("WRAMIREZ").get());
+        System.out.println(Ctx.getInstance().getOrderByAccntAndId("WRAMIREZ", 1).get());
+        System.out.println(Ctx.getInstance().putOrderByAccntAndId("WRAMIREZ", 1, 4).get());
     }
 }
