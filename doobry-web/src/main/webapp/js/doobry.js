06fd220acd3dd7257ac86a7b23116866993f6e90
Mark Aylett <mark.aylett@gmail.com>
2014-11-19 14:33:25 +0000

    Trades and books.

--- doobry-web/src/main/webapp/js/doobry.js
+++ doobry-web/src/main/webapp/js/doobry.js
@@ -48,6 +48,26 @@ ko.bindingHandlers.mnem = {
 };
 
 function Book(val, contrs) {
+
+    var contr = contrs[val.contr];
+
+    this.id = ko.observable(val.id);
+    this.contr = ko.observable(contr);
+    this.settlDate = ko.observable(val.settlDate);
+    this.bidTicks = ko.observable(val.bidTicks[0]);
+    this.bidLots = ko.observable(val.bidLots[0]);
+    this.bidCount = ko.observable(val.bidCount[0]);
+    this.offerTicks = ko.observable(val.offerTicks[0]);
+    this.offerLots = ko.observable(val.offerLots[0]);
+    this.offerCount = ko.observable(val.offerCount[0]);
+
+    this.bidPrice = ko.computed(function() {
+        return ticksToPrice(this.bidTicks(), this.contr());
+    }, this);
+
+    this.offerPrice = ko.computed(function() {
+        return ticksToPrice(this.offerTicks(), this.contr());
+    }, this);
 }
 
 function Order(val, contrs) {
@@ -81,6 +101,38 @@ function Order(val, contrs) {
 }
 
 function Trade(val, contrs) {
+
+    var contr = contrs[val.contr];
+
+    this.id = ko.observable(val.id);
+    this.orderId = ko.observable(val.orderId);
+    this.user = ko.observable(val.user);
+    this.contr = ko.observable(contr);
+    this.settlDate = ko.observable(val.settlDate);
+    this.ref = ko.observable(val.ref);
+    this.state = ko.observable(val.state);
+    this.action = ko.observable(val.action);
+    this.ticks = ko.observable(val.ticks);
+    this.lots = ko.observable(val.lots);
+    this.resd = ko.observable(val.resd);
+    this.exec = ko.observable(val.exec);
+    this.lastTicks = ko.observable(val.lastTicks);
+    this.lastLots = ko.observable(val.lastLots);
+    this.minLots = ko.observable(val.minLots);
+
+    this.matchId = ko.observable(val.matchId);
+    this.role = ko.observable(val.role);
+    this.cpty = ko.observable(val.cpty);
+
+    this.created = ko.observable(val.created);
+
+    this.price = ko.computed(function() {
+        return ticksToPrice(this.ticks(), this.contr());
+    }, this);
+
+    this.lastPrice = ko.computed(function() {
+        return ticksToPrice(this.lastTicks(), this.contr());
+    }, this);
 }
 
 function Posn(val, contrs) {
