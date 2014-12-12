b850f2f8ed92e2298b2464a8f91545e565d3ee42
Mark Aylett <mark.aylett@gmail.com>
2014-12-12 14:32:14 +0000

    Fixed unit-test.

--- swirly-elm/src/test/java/com/swirlycloud/domain/MarketTest.java
+++ swirly-elm/src/test/java/com/swirlycloud/domain/MarketTest.java
@@ -40,36 +40,6 @@ public final class MarketTest {
                 now), now);
 
         final StringBuilder sb = new StringBuilder();
-
-        // Default to TOB.
-        market.toJson(sb);
-        assertEquals(
-                "{\"id\":803163,\"contr\":\"EURUSD\",\"settlDate\":20140314,\"expiryDate\":20140314,\"bidTicks\":12344,\"bidLots\":5,\"bidCount\":1,\"offerTicks\":12346,\"offerLots\":7,\"offerCount\":2,\"lastTicks\":null,\"lastLots\":null,\"lastTime\":null}",
-                sb.toString());
-
-        // Explicit TOB.
-        sb.setLength(0);
-        market.toJson(sb);
-        assertEquals(
-                "{\"id\":803163,\"contr\":\"EURUSD\",\"settlDate\":20140314,\"expiryDate\":20140314,\"bidTicks\":12344,\"bidLots\":5,\"bidCount\":1,\"offerTicks\":12346,\"offerLots\":7,\"offerCount\":2,\"lastTicks\":null,\"lastLots\":null,\"lastTime\":null}",
-                sb.toString());
-
-        // Round-up to minimum.
-        sb.setLength(0);
-        market.toJson(sb);
-        assertEquals(
-                "{\"id\":803163,\"contr\":\"EURUSD\",\"settlDate\":20140314,\"expiryDate\":20140314,\"bidTicks\":12344,\"bidLots\":5,\"bidCount\":1,\"offerTicks\":12346,\"offerLots\":7,\"offerCount\":2,\"lastTicks\":null,\"lastLots\":null,\"lastTime\":null}",
-                sb.toString());
-
-        // Somewhere between minimum and maximum.
-        sb.setLength(0);
-        market.toJson(sb);
-        assertEquals(
-                "{\"id\":803163,\"contr\":\"EURUSD\",\"settlDate\":20140314,\"expiryDate\":20140314,\"bidTicks\":[12344,12343,null],\"bidLots\":[5,10,null],\"bidCount\":[1,1,null],\"offerTicks\":[12346,null,null],\"offerLots\":[7,null,null],\"offerCount\":[2,null,null],\"lastTicks\":null,\"lastLots\":null,\"lastTime\":null}",
-                sb.toString());
-
-        // Round-down to maximum.
-        sb.setLength(0);
         market.toJson(sb);
         assertEquals(
                 "{\"id\":803163,\"contr\":\"EURUSD\",\"settlDate\":20140314,\"expiryDate\":20140314,\"bidTicks\":[12344,12343,null,null,null],\"bidLots\":[5,10,null,null,null],\"bidCount\":[1,1,null,null,null],\"offerTicks\":[12346,null,null,null,null],\"offerLots\":[7,null,null,null,null],\"offerCount\":[2,null,null,null,null],\"lastTicks\":null,\"lastLots\":null,\"lastTime\":null}",
