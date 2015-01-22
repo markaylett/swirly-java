d89d57877f49148db3c42dca9eb9b2e8b282d18f
Mark Aylett <mark.aylett@gmail.com>
2015-01-22 21:32:41 +0000

    Hashtable with String key.

---
+++ twirly-ash/src/main/java/com/swirlycloud/twirly/intrusive/HashTableString.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (C) 2013, 2015 Swirly Cloud Limited. All rights reserved.
+ *******************************************************************************/
+package com.swirlycloud.twirly.intrusive;
+
+/**
+ * Hashtable with key of type String.
+ * 
+ * @param <T>
+ *            The concrete element type.
+ */
+public abstract class HashTableString<T> extends HashTable<T> {
+
+    protected abstract boolean equalKeys(T lhs, String rhs);
+
+    public HashTableString(int capacity) {
+        super(capacity);
+    }
+
+    public final T remove(String key) {
+        if (isEmpty()) {
+            return null;
+        }
+        int i = indexFor(key.hashCode(), buckets.length);
+        T it = getBucket(i);
+        if (it == null) {
+            return null;
+        }
+        // Check if the first element in the bucket has an equivalent key.
+        if (equalKeys(it, key)) {
+            buckets[i] = next(it);
+            --size;
+            return it;
+        }
+        // Check if a subsequent element in the bucket has an equivalent key.
+        for (T next; (next = next(it)) != null; it = next) {
+            if (equalKeys(next, key)) {
+                setNext(it, next(next));
+                --size;
+                return next;
+            }
+        }
+        return null;
+    }
+
+    public final T find(String key) {
+        if (isEmpty()) {
+            return null;
+        }
+        final int i = indexFor(key.hashCode(), buckets.length);
+        for (T it = getBucket(i); it != null; it = next(it)) {
+            if (equalKeys(it, key)) {
+                return it;
+            }
+        }
+        return null;
+    }
+}
--- twirly-ash/src/test/java/com/swirlycloud/twirly/intrusive/HashTableLongTest.java
+++ twirly-ash/src/test/java/com/swirlycloud/twirly/intrusive/HashTableLongTest.java
@@ -112,6 +112,34 @@ public final class HashTableLongTest {
     }
 
     @Test
+    public final void testRemove() {
+
+        final Entry marayl = new Entry(1, "Mark");
+        final Entry gosayl = new Entry(2, "Goska");
+        final Entry tobayl = new Entry(3, "Toby");
+        final Entry emiayl = new Entry(4, "Emily");
+
+        final EntryHashTable t = new EntryHashTable();
+        t.insert(marayl);
+        t.insert(gosayl);
+        t.insert(tobayl);
+        t.insert(emiayl);
+
+        assertSame(marayl, t.remove(1));
+        assertEquals(3, t.size());
+        assertSame(gosayl, t.remove(2));
+        assertEquals(2, t.size());
+        assertSame(tobayl, t.remove(3));
+        assertEquals(1, t.size());
+        assertSame(emiayl, t.remove(4));
+        assertTrue(t.isEmpty());
+        assertEquals(0, t.size());
+        assertNull(t.remove(5));
+        assertTrue(t.isEmpty());
+        assertEquals(0, t.size());
+    }
+
+    @Test
     public final void testFind() {
 
         final Entry marayl = new Entry(1, "Mark");
