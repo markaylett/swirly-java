5debec961d165416eb9ba7c5f7ad4668922fed60
Mark Aylett <mark.aylett@gmail.com>
2015-02-20 21:16:57 +0000

    Fixed parameter names.

--- twirly-elm/src/main/java/com/swirlycloud/twirly/domain/Conv.java
+++ twirly-elm/src/main/java/com/swirlycloud/twirly/domain/Conv.java
@@ -11,44 +11,44 @@ public final class Conv {
         return (double) numer / (double) denom;
     }
 
-    public static long realToIncs(double real, double inc_size) {
-        return Math.round(real / inc_size);
+    public static long realToIncs(double real, double incSize) {
+        return Math.round(real / incSize);
     }
 
-    public static double incsToReal(long incs, double inc_size) {
-        return incs * inc_size;
+    public static double incsToReal(long incs, double incSize) {
+        return incs * incSize;
     }
 
     /**
      * Convert quantity to lots.
      */
 
-    public static long qtyToLots(double qty, double qty_inc) {
-        return realToIncs(qty, qty_inc);
+    public static long qtyToLots(double qty, double qtyInc) {
+        return realToIncs(qty, qtyInc);
     }
 
     /**
      * Convert lots to quantity.
      */
 
-    public static double lotsToQty(long lots, double qty_inc) {
-        return incsToReal(lots, qty_inc);
+    public static double lotsToQty(long lots, double qtyInc) {
+        return incsToReal(lots, qtyInc);
     }
 
     /**
      * Convert price to ticks.
      */
 
-    public static long priceToTicks(double price, double price_inc) {
-        return realToIncs(price, price_inc);
+    public static long priceToTicks(double price, double priceInc) {
+        return realToIncs(price, priceInc);
     }
 
     /**
      * Convert ticks to price.
      */
 
-    public static double ticksToPrice(long ticks, double price_inc) {
-        return incsToReal(ticks, price_inc);
+    public static double ticksToPrice(long ticks, double priceInc) {
+        return incsToReal(ticks, priceInc);
     }
 
     /**
