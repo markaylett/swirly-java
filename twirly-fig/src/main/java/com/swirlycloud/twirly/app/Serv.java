c3501cdb5b4181c982c9bf3d0f7137a4dd824bbe
Mark Aylett <mark.aylett@gmail.com>
2015-12-07 07:15:22 +0000

    Fix removeQuote bug.

--- twirly-fig/src/main/java/com/swirlycloud/twirly/app/Serv.java
+++ twirly-fig/src/main/java/com/swirlycloud/twirly/app/Serv.java
@@ -412,7 +412,7 @@ public @NonNullByDefault class Serv {
 
     private final void removeQuote(TraderSess sess, MarketBook book, Quote quote) {
 
-        final Order order = quote.getOrder();
+        final Order order = quote.removeOrder();
         assert order != null;
 
         setDirty(sess, TraderSess.DIRTY_QUOTE);
@@ -1527,9 +1527,8 @@ public @NonNullByDefault class Serv {
 
         // Commit phase.
 
-        insertQuote(sess, book, quote);
-
         setDirty(DIRTY_TIMEOUT);
+        insertQuote(sess, book, quote);
         quotes.add(quote);
 
         updateDirty();
@@ -1592,29 +1591,30 @@ public @NonNullByDefault class Serv {
             final MarketBook book = (MarketBook) markets.find(quote.getMarket());
             assert book != null;
 
-            final Order order = quote.getOrder();
-            assert order != null;
-
             setDirty(DIRTY_TIMEOUT);
-            removeQuote(sess, book, quote);
-            quotes.removeFirst();
 
-            if (order.isPecan() && order.getQuotd() == 0) {
+            final Order order = quote.getOrder();
+            if (order != null) {
+                removeQuote(sess, book, quote);
+                if (order.isPecan() && order.getQuotd() == 0) {
 
-                // N.B. we could theoretically throw here if the allocation fails. This would
-                // result in executions not being written to the journal. The executions would be
-                // recovered, however, when the application is restarted.
-                final Exec exec = newExec(book, order, now);
+                    // N.B. we could theoretically throw here if the allocation fails. This would
+                    // result in executions not being written to the journal. The executions would
+                    // be
+                    // recovered, however, when the application is restarted.
+                    final Exec exec = newExec(book, order, now);
 
-                // Commit phase.
-                setDirty(sess, TraderSess.DIRTY_ORDER);
-                exec.cancel(0);
-                order.cancel(now);
+                    // Commit phase.
+                    setDirty(sess, TraderSess.DIRTY_ORDER);
+                    exec.cancel(0);
+                    order.cancel(now);
 
-                // Stack push.
-                exec.setJslNext(firstExec);
-                firstExec = exec;
+                    // Stack push.
+                    exec.setJslNext(firstExec);
+                    firstExec = exec;
+                }
             }
+            quotes.removeFirst();
         }
 
         if (firstExec != null) {
